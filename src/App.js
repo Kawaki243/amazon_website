51d989d6fab924e606a863680e60aaf89083eef6
Pham Tran Minh Duc <ducprodapchai243@gmail.com>
2023-08-29 14:02:34 +0200

    hello world

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import Virtual from './components/Virtual/Virtual';
+import './App.css';
+import Header from './components/Header/Header';
+import Hero from './components/Hero/Hero';
+import Slider from './components/Slider/Slider';
+import Products from './components/Products/Products';
+import Testimonials from './components/Testimonials/Testimonials';
+import Footer from './components/Footer/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Hero />
+      <Slider />
+      <Virtual />
+      <Products />
+      <Testimonials />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
